c5cfd8a7b66698198addc42a1cfdce67473334bc
Lê Hòa Bình <hb1234tv@gmail.com>
2024-10-01 20:45:59 +0700

    Delete folder web

--- uniexetask.web/Models/LoginViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace uniexetask.web.Models
-{
-    public class LoginViewModel
-    {
-        public required string Email { get; set; }
-        public required string Password { get; set; }
-    }
-}
--- unitask.frontend/src/App.js
+++ unitask.frontend/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginForm from './components/Login/LoginForm';
 import Home from './components/User/Home';
+import UserForm from './components/User/UserForm';
 
 function App() {
     return (
@@ -8,9 +9,11 @@ function App() {
             <Routes>
                 <Route path="/" element={<LoginForm />} />
                 <Route path="/home" element={<Home />} />
+                <Route path="/createUser" element={<UserForm />} />
             </Routes>
         </Router>
     );
 }
 
 export default App;
+
