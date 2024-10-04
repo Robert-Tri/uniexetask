94280c6466d2a9c0b3639943ea40672678e1a6ff
Tran Hong Hung <bintran7121@gmail.com>
2024-10-04 15:24:27 +0700

    update update User

---
+++ uniexetask.api/Models/Request/UserUpdateModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class UserUpdateModel
+{
+    [Required]
+    public int UserId { get; set; }
+
+    [Required]
+    [StringLength(100, ErrorMessage = "Full Name can't be longer than 100 characters.")]
+    public string FullName { get; set; } = null!;
+
+    [Required]
+    [EmailAddress(ErrorMessage = "Invalid Email Address")]
+    public string Email { get; set; } = null!;
+
+    [Phone(ErrorMessage = "Invalid Phone Number")]
+    public string? Phone { get; set; }
+
+    [Required]
+    public int CampusId { get; set; }
+
+    [Required]
+    public bool Status { get; set; }
+
+    [Required]
+    public int RoleId { get; set; }
+}
