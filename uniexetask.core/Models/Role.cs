c8b80d28271f21519ab7aa66db41d4a1a9b2e69e
Tran Hong Hung <bintran7121@gmail.com>
2024-10-06 19:07:24 +0700

    setting ignore model options

--- uniexetask.core/Models/Role.cs
+++ uniexetask.core/Models/Role.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace uniexetask.core.Models;
 
@@ -10,7 +11,7 @@ public partial class Role
     public string Name { get; set; } = null!;
 
     public string? Description { get; set; }
-
+    [JsonIgnore]
     public virtual ICollection<User> Users { get; set; } = new List<User>();
 
     public virtual ICollection<Permission> Permissions { get; set; } = new List<Permission>();
