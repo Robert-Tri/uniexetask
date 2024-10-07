f731c07cc7ccf68315ad25c897e782519c4aa1be
Lê Hòa Bình <hb1234tv@gmail.com>
2024-10-07 11:07:35 +0700

    Api Create Group With List Student

---
+++ uniexetask.api/Models/Request/StudentModel.cs
@@ -0,0 +1,17 @@
+﻿namespace uniexetask.api.Models.Request
+{
+    public class StudentModel
+    {
+        public int StudentId { get; set; }
+
+        public int UserId { get; set; }
+
+        public int LecturerId { get; set; }
+
+        public string StudentCode { get; set; } = null!;
+
+        public string Major { get; set; } = null!;
+
+        public bool IsEligible { get; set; }
+    }
+}
