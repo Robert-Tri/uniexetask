96237ee1bd9614fb9b5dd138a6f01ad4fecd02ba
Tran Hong Hung <bintran7121@gmail.com>
2024-10-08 23:01:03 +0700

    send email

---
+++ uniexetask.api/Extensions/Email.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Mail;
+using System.Net;
+
+namespace uniexetask.api.Extensions
+{
+    public class Email : IEmail
+    {
+        private readonly IConfiguration _configuration;
+        public Email(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public async Task SendEmailAsync(string email, string subject, string message)
+        {
+            try
+            {
+                var mail = _configuration["EmailSettings:Email"];
+                var password = _configuration["EmailSettings:Password"];
+                var client = new SmtpClient("smtp.gmail.com", 587)
+                {
+                    EnableSsl = true,
+                    Credentials = new NetworkCredential(mail, password)
+                };
+                await client.SendMailAsync(new MailMessage(from: mail, to: email, subject, message));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+        }
+    }
+}
---
+++ uniexetask.api/Extensions/IEmail.cs
@@ -0,0 +1,7 @@
+﻿namespace uniexetask.api.Extensions
+{
+    public interface IEmail
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
