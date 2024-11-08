d7d5b411bcc27a3ef381be4157860206e31ac41f
Lê Hòa Bình <hb1234tv@gmail.com>
2024-11-08 20:00:07 +0700

    api edit&delete myfindgroup

---
+++ uniexetask.api/Models/Request/UpdateRegMemberModel.cs
@@ -0,0 +1,10 @@
+﻿namespace uniexetask.api.Models.Request
+{
+    public class UpdateRegMemberModel
+    {
+        public int RegMemberId { get; set; }
+
+        public string Description { get; set; } = null!;
+
+    }
+}
