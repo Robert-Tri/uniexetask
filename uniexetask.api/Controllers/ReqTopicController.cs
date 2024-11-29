245a96aefdddb37a6acc76ee00289440e9d4db05
Lê Hòa Bình <hb1234tv@gmail.com>
2024-11-29 18:54:59 +0700

    fix review 3.3

--- uniexetask.api/Controllers/ReqTopicController.cs
+++ uniexetask.api/Controllers/ReqTopicController.cs
@@ -447,7 +447,7 @@ namespace uniexetask.api.Controllers
 
         [Authorize(Roles = "Student")]
         [HttpPut]
-        public async Task<IActionResult> UpdateReqTopic([FromBody] UpdateRegTopicModel reqTopic)
+        public async Task<IActionResult> UpdateReqTopic(IFormFile file, string topicName, int regTopicId)
         {
             var userIdString = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
             if (string.IsNullOrEmpty(userIdString) || !int.TryParse(userIdString, out int userId))
@@ -462,15 +462,18 @@ namespace uniexetask.api.Controllers
                 return BadRequest(new ApiResponse<object> { Success = false, ErrorMessage = "You are not a leader to perform this operation." });
             }
 
-            var reqNew = await _reqTopicService.GetReqTopicById(reqTopic.RegTopicId);
+            var reqNew = await _reqTopicService.GetReqTopicById(regTopicId);
 
             if (reqNew == null)
             {
                 return NotFound(new ApiResponse<object> { Success = false, ErrorMessage = "Không tìm thấy yêu cầu với ID đã cho." });
             }
 
-            reqNew.Description = reqTopic.Description;
-            reqNew.TopicName = reqTopic.TopicName;
+            if (file == null || file.Length == 0)
+                return BadRequest("No file uploaded.");
+
+            reqNew.Description = $"Topic{reqNew.GroupId}/{file.FileName}";
+            reqNew.TopicName = topicName;
             var isReqUpdated = await _reqTopicService.UpdateReqTopic(reqNew);
 
             ApiResponse<object> response = new ApiResponse<object>
